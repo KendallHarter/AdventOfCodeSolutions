0faf7e949aabbf1b9fd62bf000d3cc0b20769296
Kendall Harter <kendallremna@protonmail.com>
2020-12-06 18:58:32 -0800

    2019 day 6

--- 2019/day6.hs
+++ 2019/day6.hs
@@ -1,11 +1,18 @@
-import Common ( splitBy )
-import Data.List ( foldl', sort )
+import Common ( splitBy, extractMaybe )
+import Data.List ( foldl', sort, elemIndex )
 
--- This is correct, but far too slow and doesn't work well with the second part
--- Need to touch this up
-countOrbit :: [(String, String)] -> String -> Int
-countOrbit orbits obj = length objectsOrbited + foldl' (+) 0 (countOrbit orbits <$> objectsOrbited)
-   where objectsOrbited = fst <$> filter ((==obj) . snd) orbits
+solveOrbitsInternal :: [(String, String)] -> String -> Int -> [String]  -> (Int, [String], [String])
+solveOrbitsInternal edges node count nodePath = (numObjsOrbiting, retYouPath, retSanPath)
+   where objsOrbiting    = snd <$> filter ((==node) . fst) edges
+         retYouPath      = if node == "YOU" then nodePath else getHeadOrEmpty $ getNextPath (\(_, x, _) -> x)
+         retSanPath      = if node == "SAN" then nodePath else getHeadOrEmpty $ getNextPath (\(_, _, x) -> x)
+         getNextPath     = \f -> filter (not . null) $ fmap f nextResults
+         getHeadOrEmpty  = \x -> if null x then [] else head x
+         numObjsOrbiting = foldl' (+) count $ fmap (\(x, _, _) -> x) nextResults
+         nextResults     = (\x -> solveOrbitsInternal edges x (count + 1) (node:nodePath)) <$> objsOrbiting
+
+solveOrbits :: [(String, String)] -> String -> (Int, [String], [String])
+solveOrbits edges root = solveOrbitsInternal edges root 0 []
 
 main :: IO ()
 main = do
@@ -15,5 +22,13 @@ main = do
    let dropName = False:(uncurry (==) <$> zip (tail edgeNamesAll) edgeNamesAll)
    let edgeNames = snd <$> filter (not . fst) (zip dropName edgeNamesAll)
    let edges = (\[x, y] -> (x, y)) <$> edgesBase
+   let hasDependency = \x -> elem x $ fmap snd edges
+   let root = snd $ head $ filter (not . fst) $ zip (hasDependency <$> edgeNames) edgeNames
+   let (numOrbits, youPath, sanPath) = solveOrbits edges root
    putStr "Part 1: "
-   print $ foldl' (+) 0 $ countOrbit edges <$> edgeNames
+   print numOrbits
+   -- Find first common parent for both paths and add number of step for both to it
+   let firstShared = head $ filter (`elem` sanPath) youPath
+   let pathIndex = extractMaybe . elemIndex firstShared
+   putStr "Part 2: "
+   print $ pathIndex youPath + pathIndex sanPath
