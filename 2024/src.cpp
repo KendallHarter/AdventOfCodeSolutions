865af7f4afc01f98ec0bc6512d4046ddd17563be
Kendall Harter <kendallremna@protonmail.com>
2024-12-06 22:07:07 -0800

    2024 day 3

--- 2024/src.cpp
+++ 2024/src.cpp
@@ -179,10 +179,66 @@ void day2(const std::string& input)
    print("Part 2: {}\n", part2);
 }
 
+void day3(const std::string& input)
+{
+   const auto mul_parser = khparse::seq{
+      khparse::drop<"mul\\(">,
+      khparse::i64,
+      khparse::drop<",">,
+      khparse::i64,
+      khparse::drop<"\\)">
+   };
+   const auto do_parser = khparse::drop<"do\\(\\)">;
+   const auto do_not_parser = khparse::drop<"don't\\(\\)">;
+   std::int64_t part1 = 0;
+   for (const char* str = input.c_str(); str != input.c_str() + input.size();) {
+      const auto res = mul_parser.parse(std::string_view{str, input.c_str() + input.size()});
+      if (res) {
+         const auto [val1, val2] = res.value().value;
+         part1 += val1 * val2;
+         str = res.value().rest;
+      }
+      else {
+         ++str;
+      }
+   }
+   print("Part 1: {}\n", part1);
+
+   std::int64_t part2 = 0;
+   bool enabled = true;
+   for (const char* str = input.c_str(); str != input.c_str() + input.size();) {
+      const auto sub_input = std::string_view{str, input.c_str() + input.size()};
+      if (enabled) {
+         if (const auto res = do_not_parser.parse(sub_input)) {
+            enabled = false;
+            str = res.value().rest;
+         }
+         else if (const auto res = mul_parser.parse(sub_input)) {
+            const auto [val1, val2] = res.value().value;
+            part2 += val1 * val2;
+            str = res.value().rest;
+         }
+         else {
+            ++str;
+         }
+      }
+      else {
+         if (const auto res = do_parser.parse(sub_input)) {
+            enabled = true;
+            str = res.value().rest;
+         }
+         else {
+            ++str;
+         }
+      }
+   }
+   print("Part 2: {}\n", part2);
+}
+
 int main(int argc, const char** argv)
 {
    using func = void(*)(const std::string&);
-   constexpr func days[] = {day1, day2};
+   constexpr func days[] = {day1, day2, day3};
    constexpr int num_days = std::ssize(days);
    if (argc != 2) {
       eprint("Usage: {} day\n", argv[0]);
