451540f9de352b6ea613e3ae1414c63f48e4ab30
Kendall Harter <kendallremna@protonmail.com>
2024-12-06 23:02:19 -0800

    2024 day 4

--- 2024/src.cpp
+++ 2024/src.cpp
@@ -50,7 +50,7 @@ std::string read_file(const char* file_name)
    return to_ret;
 }
 
-void day1(const std::string& input)
+std::pair<std::int64_t, std::int64_t> day1(const std::string& input)
 {
    const auto parser = khparse::repeat{
       khparse::seq{
@@ -83,7 +83,6 @@ void day1(const std::string& input)
          return total + std::abs(vals.first - vals.second);
       }
    );
-   print("Part 1: {}\n", part1);
 
    std::unordered_map<std::int64_t, std::int64_t> number_counts;
    for (const auto& val : list2) {
@@ -96,10 +95,11 @@ void day1(const std::string& input)
          return total + val * number_counts[val];
       }
    );
-   print("Part 2: {}\n", part2);
+
+   return {part1, part2};
 }
 
-void day2(const std::string& input)
+std::pair<std::int64_t, std::int64_t> day2(const std::string& input)
 {
    const auto parser = khparse::repeat{
       khparse::seq{
@@ -153,7 +153,6 @@ void day2(const std::string& input)
          part1 += 1;
       }
    }
-   print("Part 1: {}\n", part1);
 
    int part2 = 0;
    for (const auto& line : result.value().value) {
@@ -176,10 +175,11 @@ void day2(const std::string& input)
          part2 += 1;
       }
    }
-   print("Part 2: {}\n", part2);
+
+   return {part1, part2};
 }
 
-void day3(const std::string& input)
+std::pair<std::int64_t, std::int64_t> day3(const std::string& input)
 {
    const auto mul_parser = khparse::seq{
       khparse::drop<"mul\\(">,
@@ -202,7 +202,6 @@ void day3(const std::string& input)
          ++str;
       }
    }
-   print("Part 1: {}\n", part1);
 
    std::int64_t part2 = 0;
    bool enabled = true;
@@ -232,13 +231,102 @@ void day3(const std::string& input)
          }
       }
    }
-   print("Part 2: {}\n", part2);
+
+   return {part1, part2};
+}
+
+std::pair<std::int64_t, std::int64_t> day4(const std::string& input)
+{
+   const auto parser = khparse::repeat{
+      khparse::seq{
+         khparse::capture<"[^\\n]+">,
+         khparse::drop<"\\n">
+      }
+   };
+   const auto res = parser.parse(input);
+   if (!res) {
+      eprint("Parse error!\n");
+      std::exit(1);
+   }
+   const auto grid = res.value().value;
+   int part1 = 0;
+   for (int y = 0; y < grid.size(); ++y) {
+      for (int x = 0; x < grid[y].size(); ++x) {
+         // forward
+         if (grid[y].substr(x, 4) == "XMAS") {
+            part1 += 1;
+         }
+         // forward backwards
+         if (grid[y].substr(x, 4) == "SAMX") {
+            part1 += 1;
+         }
+         if (y + 3 < grid.size()) {
+            // down
+            if (grid[y][x] == 'X' && grid[y + 1][x] == 'M' && grid[y + 2][x] == 'A' && grid[y + 3][x] == 'S') {
+               part1 += 1;
+            }
+            // down backwards
+            if (grid[y][x] == 'S' && grid[y + 1][x] == 'A' && grid[y + 2][x] == 'M' && grid[y + 3][x] == 'X') {
+               part1 += 1;
+            }
+
+            if (x + 3 < grid[y].size()) {
+               // diagonal right-down
+               if (grid[y][x] == 'X' && grid[y + 1][x + 1] == 'M' && grid[y + 2][x + 2] == 'A' && grid[y + 3][x + 3] == 'S') {
+                  part1 += 1;
+               }
+               // diagonal right-down backwards
+               if (grid[y][x] == 'S' && grid[y + 1][x + 1] == 'A' && grid[y + 2][x + 2] == 'M' && grid[y + 3][x + 3] == 'X') {
+                  part1 += 1;
+               }
+            }
+
+            if (x - 3 >= 0) {
+               // diagonal left-down
+               if (grid[y][x] == 'X' && grid[y + 1][x - 1] == 'M' && grid[y + 2][x - 2] == 'A' && grid[y + 3][x - 3] == 'S') {
+                  part1 += 1;
+               }
+               // diagonal left-down backwards
+               if (grid[y][x] == 'S' && grid[y + 1][x - 1] == 'A' && grid[y + 2][x - 2] == 'M' && grid[y + 3][x - 3] == 'X') {
+                  part1 += 1;
+               }
+            }
+         }
+      }
+   }
+
+   int part2 = 0;
+   for (int y = 1; y < grid.size() - 1; ++y) {
+      for (int x = 1; x < grid[y].size() - 1; ++x) {
+         if (grid[y][x] == 'A') {
+            const auto upper_left = grid[y - 1][x - 1];
+            const auto upper_right = grid[y - 1][x + 1];
+            const auto lower_left = grid[y + 1][x - 1];
+            const auto lower_right = grid[y + 1][x + 1];
+
+            if (upper_left == 'M' && upper_right == 'S' && lower_left == 'M' && lower_right == 'S') {
+               part2 += 1;
+            }
+            else if (upper_left == 'S' && upper_right == 'S' && lower_left == 'M' && lower_right == 'M') {
+               part2 += 1;
+            }
+            else if (upper_left == 'M' && upper_right == 'M' && lower_left == 'S' && lower_right == 'S') {
+               part2 += 1;
+            }
+            else if (upper_left == 'S' && upper_right == 'M' && lower_left == 'S' && lower_right == 'M') {
+               part2 += 1;
+            }
+         }
+      }
+   }
+
+   return {part1, part2};
 }
 
 int main(int argc, const char** argv)
 {
-   using func = void(*)(const std::string&);
-   constexpr func days[] = {day1, day2, day3};
+   using func = std::pair<std::int64_t, std::int64_t>(*)(const std::string&);
+   constexpr func days[] = {day1, day2, day3, day4};
    constexpr int num_days = std::ssize(days);
    if (argc != 2) {
       eprint("Usage: {} day\n", argv[0]);
@@ -248,5 +336,7 @@ int main(int argc, const char** argv)
       eprint("Invalid day {}\n", day);
    }
    const auto file_name = std::format("input/day{}.txt", day);
-   days[day - 1](read_file(file_name.c_str()));
+   const auto [part1, part2] = days[day - 1](read_file(file_name.c_str()));
+
+   print("Day {}\nPart 1: {}\nPart 2: {}\n", day, part1, part2);
 }
